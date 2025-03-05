e9cb453423ac357a96f602b9aa92876a0d75d6b1
Tuncay KINALI <tkinali@gmail.com>
2025-03-05 12:40:32 +0300

    bugs fixed

--- src/extension.ts
+++ src/extension.ts
@@ -3,18 +3,20 @@ import { Application } from './application';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    const app = new Application(context);
     const getterCommand = vscode.commands.registerCommand('phpgsg.generateGetters', () => {
+        const app = new Application(context);
         app.run(true, false);
     });
     context.subscriptions.push(getterCommand);
 
     const setterCommand = vscode.commands.registerCommand('phpgsg.generateSetters', () => {
+        const app = new Application(context);
         app.run(false, true);
     });
     context.subscriptions.push(setterCommand);
 
     const getterSetterCommand = vscode.commands.registerCommand('phpgsg.generateGettersAndSetters', () => {
+        const app = new Application(context);
         app.run(true, true);
     });
     context.subscriptions.push(getterSetterCommand);
